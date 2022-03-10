b71ab93526e46d31c5199d986774df65b6699e34
jaeson-dev <jaeson.dev@gmail.com>
2022-03-10 17:08:30 +0900

    Initial Commit

---
+++ RandomUserApi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomUserApi
+//
+//  Created by Jaeson.dev on 2022/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomUserApi/RandomUserApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApiApp.swift
+//  RandomUserApi
+//
+//  Created by Jaeson.dev on 2022/03/10.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
